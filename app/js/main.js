33f8896b95d3af649e4edb40035976b6518ca21b
yanam-dev <yamusjko@gmail.com>
2019-09-18 23:48:05 +0200

    added products slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,19 @@
 $(function(){
 
-    var mixer = mixitup('.products__box');
+$(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "12px",
+    // readOnly: true
+    });
+$('.products-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 4
+});
 
+    var mixer = mixitup('.products__box');
+    
+   
     
 });
