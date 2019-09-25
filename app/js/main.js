8db05fd765bee0e50e2da6b247b8c41368e0b363
yanam-dev <yamusjko@gmail.com>
2019-09-26 00:49:01 +0200

    added aside block of New items page

--- app/js/main.js
+++ app/js/main.js
@@ -12,6 +12,17 @@ $('.products-slider__inner').slick({
     slidesToScroll: 4
 });
 
+// Price Range block
+$(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1200,
+    from: 0,
+    to: 600,
+    prefix: "$",
+    grid: true
+});
+
     var mixer = mixitup('.products__box');
     
    
