6119f38c79f2e2001428b7a5dcaaddafd97c3071
yanam-dev <yamusjko@gmail.com>
2019-10-02 14:04:24 +0200

    added product-one page

--- app/js/main.js
+++ app/js/main.js
@@ -39,6 +39,16 @@ $('.menu__btn').on('click', function (){
     $('.menu__list').slideToggle();
 });
 
+// tabs
+$('.product-one__tabs .tab').on('click', function(event) {
+    var id = $(this).attr('data-id');
+    $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+    $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+    $(this).addClass('active');
+    $('#'+id).addClass('active-tab').fadeIn();
+    return false;
+    });
+
 
     var mixer = mixitup('.products__box');
     
