a8cbd70b7670d03caedde60c6671b6d343a60125
yanam-dev <yamusjko@gmail.com>
2019-10-14 10:39:23 +0200

    adaptation of the Main Page and Login popup

--- app/js/main.js
+++ app/js/main.js
@@ -9,7 +9,38 @@ $('.products-slider__inner').slick({
     dots: true,
     arrows: false,
     slidesToShow: 4,
-    slidesToScroll: 4
+    slidesToScroll: 4,
+    responsive: [
+        {
+          breakpoint: 1950,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 1490,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 721,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: false,
+            autoplay: true,
+            arrows: true
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
 });
 
 // Price Range block
